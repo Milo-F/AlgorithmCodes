c96e5a9aa949f0f2348d9861750151988aaa9e6e
Milo-F <milo.fox168@gmail.com>
2020-07-09 09:26:46 +0800

    add other solution

--- LeetCode/7.整数反转.java
+++ LeetCode/7.整数反转.java
@@ -33,5 +33,26 @@ class Solution {
         return ans;
     }
 }
+/* 
+class Solution {
+    public int reverse(int x) {
+        int ans = 0;
+        while (x != 0) {
+            int pop = x % 10;
+            if (ans > Integer.MAX_VALUE / 10 || (ans == Integer.MAX_VALUE / 10 && pop > 7)) 
+                return 0;
+            if (ans < Integer.MIN_VALUE / 10 || (ans == Integer.MIN_VALUE / 10 && pop < -8)) 
+                return 0;
+            ans = ans * 10 + pop;
+            x /= 10;
+        }
+        return ans;
+    }
+}
+作者：guanpengchn
+链接：https://leetcode-cn.com/problems/reverse-integer/solution/hua-jie-suan-fa-7-zheng-shu-fan-zhuan-by-guanpengc/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+ */
 // @lc code=end
 
