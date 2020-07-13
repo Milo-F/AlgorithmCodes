5c74e707a7ede940e115a80646e48e967ce6f9b9
Milo-F <milo.fox168@gmail.com>
2020-07-13 10:27:29 +0800

    better solution

--- LeetCode/9.回文数.java
+++ LeetCode/9.回文数.java
@@ -5,19 +5,42 @@
  */
 
 // @lc code=start
+// class Solution {
+//     public boolean isPalindrome(int x) {
+//         if (x < 0) return false;
+//         ArrayList<Integer> num = new ArrayList<>();
+//         while (x != 0) {
+//             num.add(x % 10);
+//             x = x / 10;
+//         }
+//         int len = num.size();
+//         for (int i = 0, j = len - 1; i < j; ++i, --j) {
+//             if (num.get(i) != num.get(j)) return false;
+//         }
+//         return true;
+//     }
+// }
 class Solution {
     public boolean isPalindrome(int x) {
-        if (x < 0) return false;
-        ArrayList<Integer> num = new ArrayList<>();
-        while (x != 0) {
-            num.add(x % 10);
-            x = x / 10;
+        // 特殊情况：
+        // 如上所述，当 x < 0 时，x 不是回文数。
+        // 同样地，如果数字的最后一位是 0，为了使该数字为回文，
+        // 则其第一位数字也应该是 0
+        // 只有 0 满足这一属性
+        if (x < 0 || (x % 10 == 0 && x != 0)) {
+            return false;
         }
-        int len = num.size();
-        for (int i = 0, j = len - 1; i < j; ++i, --j) {
-            if (num.get(i) != num.get(j)) return false;
+
+        int revertedNumber = 0;
+        while (x > revertedNumber) {
+            revertedNumber = revertedNumber * 10 + x % 10;
+            x /= 10;
         }
-        return true;
+
+        // 当数字长度为奇数时，我们可以通过 revertedNumber/10 去除处于中位的数字。
+        // 例如，当输入为 12321 时，在 while 循环的末尾我们可以得到 x = 12，revertedNumber = 123，
+        // 由于处于中位的数字不影响回文（它总是与自己相等），所以我们可以简单地将其去除。
+        return x == revertedNumber || x == revertedNumber / 10;
     }
 }
 // @lc code=end
