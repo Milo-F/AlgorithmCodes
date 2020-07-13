3c93a542765a0b771c41a677ce5201b121e6d3dd
Milo-F <milo.fox168@gmail.com>
2020-07-13 08:55:49 +0800

    my solution

--- LeetCode/9.回文数.java
+++ LeetCode/9.回文数.java
@@ -7,7 +7,17 @@
 // @lc code=start
 class Solution {
     public boolean isPalindrome(int x) {
-
+        if (x < 0) return false;
+        ArrayList<Integer> num = new ArrayList<>();
+        while (x != 0) {
+            num.add(x % 10);
+            x = x / 10;
+        }
+        int len = num.size();
+        for (int i = 0, j = len - 1; i < j; ++i, --j) {
+            if (num.get(i) != num.get(j)) return false;
+        }
+        return true;
     }
 }
 // @lc code=end
