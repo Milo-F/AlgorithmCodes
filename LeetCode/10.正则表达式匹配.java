e997c0ecf2eabf72517f577c81699c5a1ba6452d
Milo-F <milo.fox168@gmail.com>
2020-07-15 16:27:54 +0800

    leetcode 10

--- LeetCode/10.正则表达式匹配.java
+++ LeetCode/10.正则表达式匹配.java
@@ -7,7 +7,16 @@
 // @lc code=start
 class Solution {
     public boolean isMatch(String s, String p) {
-
+        char[] sChar = s.toCharArray();
+        char[] pChar = p.toCharArray();
+        int sLen = s.length, pLen = p.length;
+        boolean[][] pd = new boolean[sLen][pLen];
+        pd[0][0] = true;
+        for (int i = 0; i < pLen; ++i) {
+            for (int j = 0; j < sLen; ++j) {
+                
+            }
+        }
     }
 }
 // @lc code=end
