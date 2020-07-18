44675214502df69075464bf2418b2deaa322ae3d
Milo-F <milo.fox168@gmail.com>
2020-07-18 09:44:36 +0800

    easy solution

--- LeetCode/11.盛最多水的容器.java
+++ LeetCode/11.盛最多水的容器.java
@@ -7,7 +7,14 @@
 // @lc code=start
 class Solution {
     public int maxArea(int[] height) {
-
+        int len = height.length;
+        int capacity = 0;
+        for (int i = 0; i < len; ++i) {
+            for (int j = i + 1; j < len; ++j) {
+                capacity = Math.max(capacity, Math.min(height[i], height[j]) * (j - i));
+            }
+        }
+        return capacity;
     }
 }
 // @lc code=end
