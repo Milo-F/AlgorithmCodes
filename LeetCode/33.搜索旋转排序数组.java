1f841b1520ca2cee3fa3cd4b3dbc6649cd539e82
Milo-F <milo.fox168@gmail.com>
2020-08-01 15:59:19 +0800

    my solution

---
+++ LeetCode/33.搜索旋转排序数组.java
@@ -0,0 +1,44 @@
+/*
+ * @lc app=leetcode.cn id=33 lang=java
+ *
+ * [33] 搜索旋转排序数组
+ */
+
+// @lc code=start
+class Solution {
+    public int search(int[] nums, int target) {
+        int len = nums.length;
+        int l = 0, r = len - 1, index = 0;
+        if (len == 0) return -1;
+        if (nums[0] == target) return 0;
+        if (nums[len - 1] == target) return len - 1;
+        while (r > l) {
+            index = (l + r) >> 1;
+            if (index == l) {
+                if (nums[r] == target) return r;
+                r = index;
+            }
+            if (index > 0 && nums[index] < nums[index - 1] && target < nums[index]) return -1;
+            if (index < len - 1 && nums[index] > nums[index + 1] && target > nums[index]) return -1;
+            if (nums[index] == target) {
+                return index;
+            }
+            if (nums[index] > nums[l]) {
+                if (target < nums[index] && target > nums[l]) {
+                    r = index;
+                } else {
+                    l = index;
+                }
+            } else {
+                if (target > nums[index] && target < nums[r]) {
+                    l = index;
+                } else {
+                    r = index;
+                }
+            }
+        }
+        return nums[index] == target ? index : -1;
+    }
+}
+// @lc code=end
+
