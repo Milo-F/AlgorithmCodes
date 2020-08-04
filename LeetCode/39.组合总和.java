3f9d66cd6f15a5ae46719c7314596c4841f75e8f
Milo-F <milo.fox168@gmail.com>
2020-08-04 10:47:05 +0800

    leetcode 39

---
+++ LeetCode/39.组合总和.java
@@ -0,0 +1,17 @@
+/*
+ * @lc app=leetcode.cn id=39 lang=java
+ *
+ * [39] 组合总和
+ */
+
+// @lc code=start
+class Solution {
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        int len = candidates.length;
+        List<List<Integer>> result = new ArrayList<>{};
+        
+        return result;
+    }
+}
+// @lc code=end
+
