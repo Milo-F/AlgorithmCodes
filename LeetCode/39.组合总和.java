329efde08d535e26d9174fc197dbd6c577f41d25
Milo-F <milo.fox168@gmail.com>
2020-08-04 15:20:43 +0800

    stupid solution

--- LeetCode/39.组合总和.java
+++ LeetCode/39.组合总和.java
@@ -8,9 +8,30 @@
 class Solution {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
         int len = candidates.length;
-        List<List<Integer>> result = new ArrayList<>{};
-        
+        List<List<Integer>> result = new ArrayList<>(0);
+        if (len == 0 || candidates == null) return result;
+        Arrays.sort(candidates);
+        List<Integer> item = new ArrayList<>();
+        findNumbers(candidates, target, result, item);
         return result;
+    }
+    public void findNumbers(int[] nums, int target, List<List<Integer>> list, List<Integer> item) {
+        int newTarget = 0;
+        for (int i = 0; i < nums.length; ++i) {
+            newTarget = target - nums[i];
+            if (newTarget > 0) {
+                item.add(nums[i]);
+                findNumbers(nums, newTarget, list, item);
+            } else if (newTarget < 0) {
+                break;
+            } else {
+                item.add(nums[i]);
+                list.add(item);
+                item.remove(item.size() - 1);
+                break;
+            }
+        }
+        if (item.size() > 0) item.remove(item.size() - 1);
     }
 }
 // @lc code=end
