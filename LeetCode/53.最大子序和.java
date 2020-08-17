bf7740f5cc812aa8d1d1c0772466366d41aa9de1
Milo-F <milo.fox168@gmail.com>
2020-08-17 19:04:08 +0800

    solution

--- LeetCode/53.最大子序和.java
+++ LeetCode/53.最大子序和.java
@@ -7,8 +7,17 @@
 // @lc code=start
 class Solution {
     public int maxSubArray(int[] nums) {
-        int len = nums.length;
-        
+        int ans = nums[0];
+        int sum = 0;
+        for(int num: nums) {
+            if(sum > 0) {
+                sum += num;
+            } else {
+                sum = num;
+            }
+            ans = Math.max(ans, sum);
+        }
+        return ans;
     }
 }
 // @lc code=end
