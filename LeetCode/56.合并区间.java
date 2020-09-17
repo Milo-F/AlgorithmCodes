0d8018b4697b398f4df76c7dc29420cfb0321153
Milo-F <milo.fox168@gmail.com>
2020-09-17 21:21:19 +0800

    my solution

--- LeetCode/56.合并区间.java
+++ LeetCode/56.合并区间.java
@@ -7,10 +7,32 @@
 // @lc code=start
 class Solution {
     public int[][] merge(int[][] intervals) {
-        if (intervals == null || intervals.length < 2>) return intervals;
-        int number = intervals.length;
-        int[] begins = new int[number];
-        
+        List<int[]> ans = new ArrayList<>();
+        if (intervals == null || intervals.length < 2)
+            return intervals;
+        Arrays.sort(intervals, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                if (o1[0] == o2[0])
+                    return o1[1] - o2[1];
+                return o1[0] - o2[0];
+            }
+        });// 自定义排序方式，重点
+        int i = 0;
+        for (int j = 1; j < intervals.length; ++j) {
+            if (intervals[i][1] >= intervals[j][0]) {
+                intervals[i][1] = Math.max(intervals[i][1], intervals[j][1]);
+            } else {
+                ans.add(intervals[i]);
+                i = j;
+            }
+        }
+        ans.add(intervals[i]);
+        int[][] result = new int[ans.size()][];
+        for(int k = 0; k < ans.size(); ++k) {
+            result[k] = ans.get(k);
+        }
+        return result;
     }
 }
 // @lc code=end
