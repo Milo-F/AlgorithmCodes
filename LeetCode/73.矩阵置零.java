a476c24b54434df5e1a23c8b4e1778285ca49d7a
Milo-F <milo.fox168@gmail.com>
2020-10-25 17:12:18 +0800

    solution

--- LeetCode/73.矩阵置零.java
+++ LeetCode/73.矩阵置零.java
@@ -5,27 +5,50 @@
  */
 
 // @lc code=start
+// class Solution {
+//     public void setZeroes(int[][] matrix) {
+//         int m = matrix.length, n = matrix[0].length;
+//         int[][] originMatrix = new int[m][n];
+//         for (int i = 0; i < m; ++i) {
+//             for (int j = 0; j < n; ++j) {
+//                 if (matrix[i][j] != 0) {
+//                     continue;
+//                 } else {
+//                     for (int k = 0; k < m; ++k) {
+//                         originMatrix[k][j] = matrix[k][j];
+//                     }
+//                     for (int k = 0; k < n; ++k) {
+//                         originMatrix[i][k] = matrix[i][k];
+//                     }
+//                 }
+//             }
+//         }
+//         for (int i = 0; i < m; ++i) {
+//             for (int j = 0; j < n; ++j) {
+//                 matrix[i][j] = matrix[i][j] - originMatrix[i][j];
+//             }
+//         }
+//     }
+// }
 class Solution {
     public void setZeroes(int[][] matrix) {
+        List<int[]> location = new ArrayList<>(1);
         int m = matrix.length, n = matrix[0].length;
-        int[][] originMatrix = new int[m][n];
         for (int i = 0; i < m; ++i) {
             for (int j = 0; j < n; ++j) {
-                if (matrix[i][j] != 0) {
-                    continue;
-                } else {
-                    for (int k = 0; k < m; ++k) {
-                        originMatrix[k][j] = matrix[k][j];
-                    }
-                    for (int k = 0; k < n; ++k) {
-                        originMatrix[i][k] = matrix[i][k];
-                    }
+                if (matrix[i][j] != 0) continue;
+                else {
+                    int[] tempLocation = {i, j};
+                    location.add(tempLocation);
                 }
             }
         }
-        for (int i = 0; i < m; ++i) {
+        for (int[] temp : location) {
+            for (int i = 0; i < m; ++i) {
+                matrix[i][temp[1]] = 0;
+            }
             for (int j = 0; j < n; ++j) {
-                matrix[i][j] = matrix[i][j] - originMatrix[i][j];
+                matrix[temp[0]][j] = 0;
             }
         }
     }
