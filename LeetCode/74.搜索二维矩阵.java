f5db075657d935308370e1a620ffb8a8c01cef62
Milo-F <milo.fox168@gmail.com>
2020-10-26 22:04:54 +0800

    暴力解法

--- LeetCode/74.搜索二维矩阵.java
+++ LeetCode/74.搜索二维矩阵.java
@@ -5,9 +5,27 @@
  */
 
 // @lc code=start
+// class Solution {
+//     public boolean searchMatrix(int[][] matrix, int target) {
+//         if (matrix.length == 0 || matrix == null) return false;
+//         int m = matrix.length, n = matrix[0].length;
+//         boolean finish = false;
+//         for (int i = 0; i < m; ++i) {
+//             for (int j = 0; j < n; ++j) {
+//                 if (matrix[i][j] > target) {
+//                     finish = true;
+//                     break;
+//                 }
+//                 if (matrix[i][j] == target) return true;
+//             }
+//             if (finish) break;
+//         }
+//         return false;
+//     }
+// }
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-
+        
     }
 }
 // @lc code=end
